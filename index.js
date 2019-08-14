85067827f55d9d588a924d6ebe82db231f710f8c
Aizul Faiz Iswafaza <izul250397@gmail.com>
2019-08-14 11:02:11 +0700

    heroku again

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(express.static(__dirname+'/public'));
 //fire controller
 todoController(app);
 
+app.get('/', function(req, res){
+    //get data mongodb
+	res.end("It's working: " + __dirname);
+});
+
 //port listener
 app.listen(port, function(){
 	console.log('Connected on port 3000');
