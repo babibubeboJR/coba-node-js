c94de9fadb9bed92c8fe826e1fdcb7c3125c1d28
Aizul Faiz Iswafaza <izul250397@gmail.com>
2019-08-14 09:21:58 +0700

    new add

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+//setup engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controller
+todoController(app);
+
+//port listener
+app.listen(3000);
+console.log('Connected on port 3000');
